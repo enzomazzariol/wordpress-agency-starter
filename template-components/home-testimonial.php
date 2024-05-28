73d8429c723b2febc87ba791522ccd197b181329
mazzariolenzo <mazzariolenzo@gmail.com>
2024-05-28 20:15:41 +0200

    add for bucles to faqs and testimonial

--- template-components/home-testimonial.php
+++ template-components/home-testimonial.php
@@ -1,12 +1,14 @@
-<!--TESTIMONIAL-->
+<!-- TESTIMONIAL -->
 <?php
 $testimonial = get_field('testimonial');
-$testimonial1 = $testimonial['testimonial_1'];
-$testimonial2 = $testimonial['testimonial_2'];
-$testimonial3 = $testimonial['testimonial_3'];
-$testimonial4 = $testimonial['testimonial_4'];
-$testimonial5 = $testimonial['testimonial_5'];
-$testimonial6 = $testimonial['testimonial_6'];
+$testimonials = [
+  $testimonial['testimonial_1'],
+  $testimonial['testimonial_2'],
+  $testimonial['testimonial_3'],
+  $testimonial['testimonial_4'],
+  $testimonial['testimonial_5'],
+  $testimonial['testimonial_6']
+];
 ?>
 
 <section class="container-fluid px-0 testimonial">
@@ -18,247 +20,71 @@ $testimonial6 = $testimonial['testimonial_6'];
     </div>
   </div>
 
-  <!-- Carousel -->
-  <div id="carouselExampleIndicators" class="carousel slide d-none d-lg-block">
+  <!-- Carousel para dispositivos grandes -->
+  <div id="carouselExampleIndicatorsLarge" class="carousel slide d-none d-lg-block">
     <div class="carousel-indicators">
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <?php for ($i = 0; $i < ceil(count($testimonials) / 2); $i++) : ?>
+        <button type="button" data-bs-target="#carouselExampleIndicatorsLarge" data-bs-slide-to="<?php echo $i; ?>" class="<?php echo $i === 0 ? 'active' : ''; ?>" aria-current="true" aria-label="Slide <?php echo $i + 1; ?>"></button>
+      <?php endfor; ?>
     </div>
     <div class="carousel-inner">
-      <!--Slider 1-->
-          <div class="carousel-item active">
-              <div class=" container">
-                <div class="row g-3">
+      <?php for ($i = 0; $i < count($testimonials); $i += 2) : ?>
+        <div class="carousel-item <?php echo $i === 0 ? 'active' : ''; ?>">
+          <div class="container">
+            <div class="row g-3">
+              <?php for ($j = 0; $j < 2; $j++) : ?>
+                <?php if (isset($testimonials[$i + $j])) : $current_testimonial = $testimonials[$i + $j]; ?>
                   <div class="col">
                     <div class="testimonial-box text-center mb-4">
                       <div class="d-flex justify-content-start p-3">
-                        <img src="<?php echo get_template_directory_uri() . "/src/assets/images/testimonial/quotes.svg" ?>"
-                          alt="quotes" class="me-1" />
+                        <img src="<?php echo get_template_directory_uri() . "/src/assets/images/testimonial/quotes.svg"; ?>" alt="quotes" class="me-1" />
                       </div>
-                      <p> <?php echo esc_html($testimonial1['opinion']); ?> </p>
+                      <p><?php echo esc_html($current_testimonial['opinion']); ?></p>
                       <div class="d-flex justify-content-end p-2 me-3">
-                        <img src="<?php echo get_template_directory_uri() . "/src/assets/images/testimonial/quotes.svg" ?>"
-                          alt="quotes" class="me-1 invert-quotes" />
+                        <img src="<?php echo get_template_directory_uri() . "/src/assets/images/testimonial/quotes.svg"; ?>" alt="quotes" class="me-1 invert-quotes" />
                       </div>
                     </div>
-                    <h5 class="mt-auto">
-                      <?php echo esc_html($testimonial1['name']); ?>
-                    </h5>
-                    <p class="mt-2"> <?php echo esc_html($testimonial1['profession']); ?> </p>
+                    <h5 class="mt-auto"><?php echo esc_html($current_testimonial['name']); ?></h5>
+                    <p class="mt-2"><?php echo esc_html($current_testimonial['profession']); ?></p>
                   </div>
-                  <div class="col">
-                    <div class="testimonial-box text-center mb-4">
-                      <div class="d-flex justify-content-start p-3">
-                        <img src="<?php echo get_template_directory_uri() . "/src/assets/images/testimonial/quotes.svg" ?>"
-                          alt="quotes" class="me-1" />
-                      </div>
-                      <p>
-                        <?php echo esc_html($testimonial2['opinion']); ?>
-                      </p>
-                      <div class="d-flex justify-content-end p-2 me-3">
-                        <img src="<?php echo get_template_directory_uri() . "/src/assets/images/testimonial/quotes.svg" ?>"
-                          alt="quotes" class="me-1 invert-quotes" />
-                      </div>
-                    </div>
-                    <h5 class="mt-auto">
-                      <?php echo esc_html($testimonial2['name']); ?>
-                    </h5>
-                    <p class="mt-2"> <?php echo esc_html($testimonial2['profession']); ?> </p>
-                  </div>
-                </div>
-              </div>
-          </div>
-        <!--Slider 2-->
-          <div class="carousel-item">
-                <div class=" container">
-                  <div class="row g-3">
-                    <div class="col">
-                      <div class="testimonial-box text-center mb-4">
-                        <div class="d-flex justify-content-start p-3">
-                          <img src="<?php echo get_template_directory_uri() . "/src/assets/images/testimonial/quotes.svg" ?>"
-                            alt="quotes" class="me-1" />
-                        </div>
-                        <p> <?php echo esc_html($testimonial3['opinion']); ?> </p>
-                        <div class="d-flex justify-content-end p-2 me-3">
-                          <img src="<?php echo get_template_directory_uri() . "/src/assets/images/testimonial/quotes.svg" ?>"
-                            alt="quotes" class="me-1 invert-quotes" />
-                        </div>
-                      </div>
-                      <h5 class="mt-auto">
-                        <?php echo esc_html($testimonial3['name']); ?>
-                      </h5>
-                      <p class="mt-2"> <?php echo esc_html($testimonial3['profession']); ?> </p>
-                    </div>
-                    <div class="col">
-                      <div class="testimonial-box text-center mb-4">
-                        <div class="d-flex justify-content-start p-3">
-                          <img src="<?php echo get_template_directory_uri() . "/src/assets/images/testimonial/quotes.svg" ?>"
-                            alt="quotes" class="me-1" />
-                        </div>
-                        <p>
-                          <?php echo esc_html($testimonial4['opinion']); ?>
-                        </p>
-                        <div class="d-flex justify-content-end p-2 me-3">
-                          <img src="<?php echo get_template_directory_uri() . "/src/assets/images/testimonial/quotes.svg" ?>"
-                            alt="quotes" class="me-1 invert-quotes" />
-                        </div>
-                      </div>
-                      <h5 class="mt-auto">
-                        <?php echo esc_html($testimonial4['name']); ?>
-                      </h5>
-                      <p class="mt-2"> <?php echo esc_html($testimonial4['profession']); ?> </p>
-                    </div>
-                  </div>
+                <?php endif; ?>
+              <?php endfor; ?>
             </div>
           </div>
+        </div>
+      <?php endfor; ?>
     </div>
-      
   </div>
 
-
-  <!--Carousel para dispositivos pequeños-->
-  <div id="carouselExampleIndicators" class="carousel slide d-lg-none d-md-block d-sm-block d-xs-block"
-    data-bs-ride="carousel" data-bs-interval="5000">
+  <!-- Carousel para dispositivos pequeños -->
+  <div id="carouselExampleIndicatorsSmall" class="carousel slide d-lg-none" data-bs-ride="carousel" data-bs-interval="5000">
+    <div class="carousel-indicators">
+      <?php for ($i = 0; $i < count($testimonials); $i++) : ?>
+        <button type="button" data-bs-target="#carouselExampleIndicatorsSmall" data-bs-slide-to="<?php echo $i; ?>" class="<?php echo $i === 0 ? 'active' : ''; ?>" aria-current="true" aria-label="Slide <?php echo $i + 1; ?>"></button>
+      <?php endfor; ?>
+    </div>
     <div class="carousel-inner">
-
-      <!-- Slider 1 -->
-      <div class="carousel-item active">
-        <div class="container">
-          <div class="row g-3">
-            <div class="col cartas">
-              <div class="testimonial-box text-center mb-4">
-                <div class="d-flex justify-content-start p-3">
-                  <img src="<?php echo get_template_directory_uri() . "/src/assets/images/testimonial/quotes.svg" ?>"
-                    alt="quotes" class="me-1" />
+      <?php for ($i = 0; $i < count($testimonials); $i++) : ?>
+        <div class="carousel-item <?php echo $i === 0 ? 'active' : ''; ?>">
+          <div class="container">
+            <div class="row g-3">
+              <div class="col-12">
+                <div class="testimonial-box text-center mb-4">
+                  <div class="d-flex justify-content-start p-3">
+                    <img src="<?php echo get_template_directory_uri() . "/src/assets/images/testimonial/quotes.svg"; ?>" alt="quotes" class="me-1" />
+                  </div>
+                  <p><?php echo esc_html($testimonials[$i]['opinion']); ?></p>
+                  <div class="d-flex justify-content-end p-2 me-3">
+                    <img src="<?php echo get_template_directory_uri() . "/src/assets/images/testimonial/quotes.svg"; ?>" alt="quotes" class="me-1 invert-quotes" />
+                  </div>
                 </div>
-                <p>
-                  <?php echo esc_html($testimonial1['opinion']); ?>
-                </p>
-                <div class="d-flex justify-content-end p-2 me-3">
-                  <img src="<?php echo get_template_directory_uri() . "/src/assets/images/testimonial/quotes.svg" ?>"
-                    alt="quotes" class="me-1 invert-quotes" />
-                </div>
+                <h5 class="text-center"><?php echo esc_html($testimonials[$i]['name']); ?></h5>
+                  <p class="mt-2 text-center"><?php echo esc_html($testimonials[$i]['profession']); ?></p>
               </div>
-              <h5 class="mt-4 text-center">
-                <?php echo esc_html($testimonial1['name']); ?>
-              </h5>
-              <p class="mt-2 text-center">
-                <?php echo esc_html($testimonial1['profession']); ?>
-              </p>
             </div>
           </div>
         </div>
-      </div>
-  
-
-    <!-- Slider 2 -->
-    <div class="carousel-item">
-      <div class="container">
-        <div class="row g-3">
-          <div class="col cartas">
-
-            <div class="testimonial-box text-center mb-4">
-              <div class="d-flex justify-content-start p-3">
-                <img src="<?php echo get_template_directory_uri() . "/src/assets/images/testimonial/quotes.svg" ?>"
-                  alt="quotes" class="me-1" />
-              </div>
-              <p>
-                <?php echo esc_html($testimonial1['opinion']); ?>
-              </p>
-              <div class="d-flex justify-content-end p-2 me-3">
-                <img src="<?php echo get_template_directory_uri() . "/src/assets/images/testimonial/quotes.svg" ?>"
-                  alt="quotes" class="me-1 invert-quotes" />
-              </div>
-            </div>
-            <h5 class="mt-4 text-center">
-              <?php echo esc_html($testimonial1['name']); ?>
-            </h5>
-            <p class="mt-2 text-center">
-              <?php echo esc_html($testimonial1['profession']); ?>
-            </p>
-          </div>
-
-        </div>
-      </div>
+      <?php endfor; ?>
     </div>
-
-  <!-- Slider 3 -->
-  <div class="carousel-item">
-    <div class="container">
-      <div class="row g-3">
-        <div class="col cartas">
-
-          <div class="testimonial-box text-center mb-4">
-            <div class="d-flex justify-content-start p-3">
-              <img src="<?php echo get_template_directory_uri() . "/src/assets/images/testimonial/quotes.svg" ?>"
-                alt="quotes" class="me-1" />
-            </div>
-            <p>
-              <?php echo esc_html($testimonial1['opinion']); ?>
-            </p>
-            <div class="d-flex justify-content-end p-2 me-3">
-              <img src="<?php echo get_template_directory_uri() . "/src/assets/images/testimonial/quotes.svg" ?>"
-                alt="quotes" class="me-1 invert-quotes" />
-            </div>
-          </div>
-          <h5 class="mt-4 text-center">
-            <?php echo esc_html($testimonial1['name']); ?>
-          </h5>
-          <p class="mt-2 text-center">
-            <?php echo esc_html($testimonial1['profession']); ?>
-          </p>
-        </div>
-
-      </div>
-    </div>
-  </div>
-
-  </div> <!--close div carousel inner - falta llenar de info los slider de abajo-->
-
-  <!-- Slider 4 -->
-  <div class="carousel-item">
-    <div class="container">
-      <div class="row g-3">
-        <div class="col cartas">
-          <!-- Contenido del testimonial 4 -->
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <!-- Slider 5 -->
-  <div class="carousel-item">
-    <div class="container">
-      <div class="row g-3">
-        <div class="col cartas">
-          <!-- Contenido del testimonial 5 -->
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <!-- Slider 6 -->
-  <div class="carousel-item">
-    <div class="container">
-      <div class="row g-3">
-        <div class="col cartas">
-          <!-- Contenido del testimonial 6 -->
-        </div>
-      </div>
-    </div>
-  </div>
-
-        <!--Buttons Carousel-->
-      <div class="d-flex justify-content-center align-items-center mt-5 d-lg-none">
-        <a href="#carouselExampleIndicators" role="button" data-bs-slide-to="0" class="buttons-slider me-4">
-          <div class="button-circle"></div>
-        </a>
-        <a href="#carouselExampleIndicators" role="button" data-bs-slide-to="1" class="buttons-slider me-4">
-          <div class="button-circle"></div>
-        </a>
-        <a href="#carouselExampleIndicators" role="button" data-bs-slide-to="2" class="buttons-slider">
-          <div class="button-circle"></div>
-        </a>
-      </div>
   </div>
 </section>
