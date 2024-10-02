a5a1e189b5397cbd67ab531d60da097bec97cc11
Walter Mazzariol <mazzariolwalter@gmail.com>
2024-10-02 23:28:46 +0200

    some changes

--- template-components/home-description.php
+++ template-components/home-description.php
@@ -13,8 +13,5 @@ $description = get_field('description');
         <?php echo esc_html($description['description_gray']); ?>
       </p>
     </div>
-    <div class="col-12 d-flex justify-content-center mb-5">
-      <a class=" button button--second">More info</a>
-    </div>
   </div>
 </section>
